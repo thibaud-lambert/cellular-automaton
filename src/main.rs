750513aa8e34b09af406861fa5ba738985c6705e
Thibaud Lambert <thibaud.lambert@gmail.com>
2015-08-13 01:34:38 +0200

    Starting to implement space.rs

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,13 @@
+mod core;
+use core::space::Grid;
+
+#[derive(Copy, Clone)]
+enum State {
+    ALIVE,
+    DEAD,
+}
+
 fn main() {
+    let g = Grid::<State>::new(5,5);
     println!("Hello, world!");
 }
